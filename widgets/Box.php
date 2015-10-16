69fdf0409daf94f0e9349e629ad77e0b78242bda
Mehdi Achour <machour@gmail.com>
2015-10-16 17:07:12 +0100

    Turn Box into a Trait to be able to skin other widgets

--- widgets/Box.php
+++ widgets/Box.php
@@ -3,78 +3,19 @@
 namespace app\widgets;
 
 use yii\base\Widget;
-use yii\helpers\ArrayHelper;
-use yii\helpers\Html;
 
-class Box extends Widget {
+class Box extends Widget  {
 
-    public static $config = [
-        'type' => 'box-solid',
-        'noPadding' => false,
-        'header' => [
-            'title' => '',
-            'class' => '',
-            'baseOptions' => [
-                'collapse' => true,
-                'remove' => true,
-            ]
-        ],
-        'footer' => '',
-    ];
+    use BoxTrait;
+
+    public static $config = [];
 
     public static function begin($config = []) {
-
-        self::$config = ArrayHelper::merge(self::$config, $config);
-
-        echo Html::beginTag('div', ['class' => 'box ' . self::$config['type']]);
-
-        if (!empty(self::$config['header']['title'])) {
-            $header = self::$config['header'];
-            echo Html::beginTag('div', ['class' => 'box-header ' . $header['class']]);
-
-            if (!empty($header['title'])) {
-                echo Html::tag('h3', Html::encode($header['title']), ['class' => 'box-title']);
-            }
-
-            if (array_sum($header['baseOptions'])) {
-                echo Html::beginTag('div', ['class' => 'box-tools pull-right']);
-
-                if ($header['baseOptions']['collapse']) {
-                    echo self::tool('collapse', 'minus');
-                }
-                if ($header['baseOptions']['remove']) {
-                    echo self::tool('remove', 'times');
-                }
-
-                echo Html::endTag('div');
-            }
-
-            echo Html::endTag('div');
-        }
-
-        $class = 'box-body';
-        if (self::$config['noPadding']) {
-            $class .= ' no-padding';
-        }
-        echo Html::beginTag('div', ['class' => $class]);
+        self::boxBegin($config);
     }
 
     public static function end() {
-        echo Html::endTag('div');
-        if (!empty(self::$config['footer'])) {
-            echo Html::beginTag('div', ['class' => 'box-footer']);
-            echo self::$config['footer'];
-            echo Html::endTag('div');
-        }
-        echo Html::endTag('div');
-    }
-
-    private static function tool($widget, $icon) {
-        return Html::tag(
-            'button',
-            Html::tag('i', null, ['class' => 'fa fa-' . $icon]),
-            ['data-widget' => $widget, 'class' => 'btn btn-box-tool']
-        );
+        self::boxEnd();
     }
 
 }
---
+++ widgets/BoxTrait.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace app\widgets;
+
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+
+trait BoxTrait {
+
+    public $box = false;
+
+    public static $boxConfig = [
+        'type' => 'box-solid',
+        'noPadding' => false,
+        'header' => [
+            'title' => '',
+            'class' => '',
+            'baseOptions' => [
+                'collapse' => true,
+                'remove' => true,
+            ]
+        ],
+        'footer' => '',
+    ];
+
+    public static function boxBegin($boxConfig = []) {
+
+        self::$boxConfig = ArrayHelper::merge(self::$boxConfig, $boxConfig);
+
+        echo Html::beginTag('div', ['class' => 'box ' . self::$boxConfig['type']]);
+
+        if (!empty(self::$boxConfig['header']['title'])) {
+            $header = self::$boxConfig['header'];
+            echo Html::beginTag('div', ['class' => 'box-header ' . $header['class']]);
+
+            if (!empty($header['title'])) {
+                echo Html::tag('h3', Html::encode($header['title']), ['class' => 'box-title']);
+            }
+
+            if (array_sum($header['baseOptions'])) {
+                echo Html::beginTag('div', ['class' => 'box-tools pull-right']);
+
+                if ($header['baseOptions']['collapse']) {
+                    echo self::boxTool('collapse', 'minus');
+                }
+                if ($header['baseOptions']['remove']) {
+                    echo self::boxTool('remove', 'times');
+                }
+
+                echo Html::endTag('div');
+            }
+
+            echo Html::endTag('div');
+        }
+
+        $class = 'box-body';
+        if (self::$boxConfig['noPadding']) {
+            $class .= ' no-padding';
+        }
+        echo Html::beginTag('div', ['class' => $class]);
+    }
+
+    public static function boxEnd() {
+        echo Html::endTag('div');
+        if (!empty(self::$boxConfig['footer'])) {
+            echo Html::beginTag('div', ['class' => 'box-footer']);
+            echo self::$boxConfig['footer'];
+            echo Html::endTag('div');
+        }
+        echo Html::endTag('div');
+    }
+
+    private static function boxTool($widget, $icon) {
+        return Html::tag(
+            'button',
+            Html::tag('i', null, ['class' => 'fa fa-' . $icon]),
+            ['data-widget' => $widget, 'class' => 'btn btn-box-tool']
+        );
+    }
+
+}
